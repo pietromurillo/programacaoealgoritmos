bff3df779d3b5d2c08b5a9d1ffc928ddae0a5a89
pietromurillo <pietromurillo19@gmail.com>
2024-07-25 12:33:07 -0300

    Add files via upload

---
+++ aula1-revisao.js
@@ -0,0 +1,13 @@
+let saudacao= "Olá mundo";
+let numero= 25;
+let decimal= 3.14;
+let verdadeiro= true;
+let falso= false;
+
+console.log("***********************")
+console.log(saudacao)
+console.log("numero", numero)
+console.log("decimal", decimal)
+console.log("verdadeiro", verdadeiro)
+console.log("falso", falso)
+console.log("***********************")
